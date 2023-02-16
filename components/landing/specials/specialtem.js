526de05d5c75df8bb56a8e10cb69e2636abf91d1
Batuhan San <batuhansann@gmail.com>
2023-02-16 20:17:03 +0300

    fixed  eslint issues

--- components/landing/specials/specialtem.js
+++ components/landing/specials/specialtem.js
@@ -8,7 +8,7 @@ export default function SpecialItem(props) {
       </div>
       <h1 className="text-base mt-4">{props.title}</h1>
       <div className="text-center px-8 text-sm mt-4 text-white/60">
-        You don't have to spend thousands of dollars to get a nice website just
+        You {"don't"} have to spend thousands of dollars to get a nice website just
         to launch your product.
       </div>
     </div>
--- components/landing/splashMockup.js
+++ components/landing/splashMockup.js
@@ -19,9 +19,8 @@ export default function SplashImage() {
             className="h-full w-full p-1"
             src="https://www.youtube.com/embed/RKW6rjnYEkc?controls=0"
             title="YouTube video player"
-            frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
+            allowFullScreen
           ></iframe>
       </div>
     </div>
