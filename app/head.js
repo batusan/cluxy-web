6b8fd0228bdc105265bc81bd78fb56b431280f09
Batuhan San <batuhansann@gmail.com>
2023-02-23 23:19:56 +0300

    favicon changed

--- app/head.js
+++ app/head.js
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Cluxy | A discord bot</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Cluxy discord bot for server managements" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon-cluxy.ico" />
     </>
   )
 }
